66430987c468dc2a79541a202cff6d9f4227d0ef
Minjune Hwang <mjhwang@DN51s4ua.SUNet>
2021-11-10 21:10:51 -0800

    add sampler and dataloader

--- chexpert_loader.py
+++ chexpert_loader.py
@@ -82,9 +82,10 @@ class ChexpertDataset(dataset.Dataset):
             torch.from_numpy(images_query), torch.from_numpy(labels_query), torch.from_numpy(mask_query)
 
     def load_image(self, im_path):
+        # TODO: Should we normalize??
         im = Image.open(im_path)
         im = im.resize(self.im_size, Image.BILINEAR)
-        return np.array(im)
+        return np.array(im/255)
 
     @staticmethod
     def random_sampling(labels, num_samples):
@@ -102,25 +103,24 @@ class ChexpertDataset(dataset.Dataset):
 
 # TODO: Code from HW2
 class ChexpertSampler(sampler.Sampler):
-    def __init__(self, split_idxs, num_way, num_tasks):
+    def __init__(self, class_idxs, num_targets_per_task, num_tasks):
         """Inits OmniglotSampler.
 
         Args:
-            split_idxs (range): indices that comprise the
-                training/validation/test split
-            num_way (int): number of classes per task
+            class_idxs (int): indices of classes (targets)
+            num_targets_per_task (range): number of targets/classes per task
             num_tasks (int): number of tasks to sample
         """
         super().__init__(None)
-        self._split_idxs = split_idxs
-        self._num_way = num_way
+        self._class_idxs = class_idxs
+        self._num_targets_per_task = num_targets_per_task
         self._num_tasks = num_tasks
 
     def __iter__(self):
         return (
             np.random.default_rng().choice(
-                self._split_idxs,
-                size=self._num_way,
+                self._class_idxs,
+                size=self._num_targets_per_task,
                 replace=False
             ) for _ in range(self._num_tasks)
         )  # generator of len num_tasks, each elem is size N array of indices
@@ -128,168 +128,18 @@ class ChexpertSampler(sampler.Sampler):
     def __len__(self):
         return self._num_tasks
 
-
-
-
-
-
-NUM_TRAIN_CLASSES = 1100
-NUM_VAL_CLASSES = 100
-NUM_TEST_CLASSES = 423
-NUM_SAMPLES_PER_CLASS = 20
-
-
-def load_image(file_path):
-    """Loads and transforms an Omniglot image.
-
-    Args:
-        file_path (str): file path of image
-
-    Returns:
-        a Tensor containing image data
-            shape (1, 28, 28)
-    """
-    x = imageio.imread(file_path)
-    x = torch.tensor(x, dtype=torch.float32).reshape([1, 28, 28])
-    x = x / 255.0
-    return 1 - x
-
-
-class OmniglotDataset(dataset.Dataset):
-    """Omniglot dataset for meta-learning.
-
-    Each element of the dataset is a task. A task is specified with a key,
-    which is a tuple of class indices (no particular order). The corresponding
-    value is the instantiated task, which consists of sampled (image, label)
-    pairs.
-    """
-
-    _BASE_PATH = './omniglot_resized'
-    _GDD_FILE_ID = '1iaSFXIYC3AB8q9K_M-oVMa4pmB7yKMtI'
-
-    def __init__(self, num_support, num_query):
-        """Inits OmniglotDataset.
-
-        Args:
-            num_support (int): number of support examples per class
-            num_query (int): number of query examples per class
-        """
-        super().__init__()
-
-
-        # if necessary, download the Omniglot dataset
-        if not os.path.isdir(self._BASE_PATH):
-            gdd.GoogleDriveDownloader.download_file_from_google_drive(
-                file_id=self._GDD_FILE_ID,
-                dest_path=f'{self._BASE_PATH}.zip',
-                unzip=True
-            )
-
-        # get all character folders
-        self._character_folders = glob.glob(
-            os.path.join(self._BASE_PATH, '*/*/'))
-        assert len(self._character_folders) == (
-            NUM_TRAIN_CLASSES + NUM_VAL_CLASSES + NUM_TEST_CLASSES
-        )
-
-        # shuffle characters
-        np.random.default_rng(0).shuffle(self._character_folders)
-
-        # check problem arguments
-        assert num_support + num_query <= NUM_SAMPLES_PER_CLASS
-        self._num_support = num_support
-        self._num_query = num_query
-
-    def __getitem__(self, class_idxs):
-        """Constructs a task.
-
-        Data for each class is sampled uniformly at random without replacement.
-        The ordering of the labels corresponds to that of class_idxs.
-
-        Args:
-            class_idxs (tuple[int]): class indices that comprise the task
-
-        Returns:
-            images_support (Tensor): task support images
-                shape (num_way * num_support, channels, height, width)
-            labels_support (Tensor): task support labels
-                shape (num_way * num_support,)
-            images_query (Tensor): task query images
-                shape (num_way * num_query, channels, height, width)
-            labels_query (Tensor): task query labels
-                shape (num_way * num_query,)
-        """
-        images_support, images_query = [], []
-        labels_support, labels_query = [], []
-
-        for label, class_idx in enumerate(class_idxs):
-            # get a class's examples and sample from them
-            all_file_paths = glob.glob(
-                os.path.join(self._character_folders[class_idx], '*.png')
-            )
-            sampled_file_paths = np.random.default_rng().choice(
-                all_file_paths,
-                size=self._num_support + self._num_query,
-                replace=False
-            )
-            images = [load_image(file_path) for file_path in sampled_file_paths]
-
-            # split sampled examples into support and query
-            images_support.extend(images[:self._num_support])
-            images_query.extend(images[self._num_support:])
-            labels_support.extend([label] * self._num_support)
-            labels_query.extend([label] * self._num_query)
-
-        # aggregate into tensors
-        images_support = torch.stack(images_support)  # shape (N*S, C, H, W)
-        labels_support = torch.tensor(labels_support)  # shape (N*S)
-        images_query = torch.stack(images_query)
-        labels_query = torch.tensor(labels_query)
-
-        return images_support, labels_support, images_query, labels_query
-
-
-class OmniglotSampler(sampler.Sampler):
-    """Samples task specification keys for an OmniglotDataset."""
-
-    def __init__(self, split_idxs, num_way, num_tasks):
-        """Inits OmniglotSampler.
-
-        Args:
-            split_idxs (range): indices that comprise the
-                training/validation/test split
-            num_way (int): number of classes per task
-            num_tasks (int): number of tasks to sample
-        """
-        super().__init__(None)
-        self._split_idxs = split_idxs
-        self._num_way = num_way
-        self._num_tasks = num_tasks
-
-    def __iter__(self):
-        return (
-            np.random.default_rng().choice(
-                self._split_idxs,
-                size=self._num_way,
-                replace=False
-            ) for _ in range(self._num_tasks)
-        )
-
-    def __len__(self):
-        return self._num_tasks
-
-
 def identity(x):
     return x
 
-
-def get_omniglot_dataloader(
-        split,
+def get_chexpert_dataloader(
+        data_path,
         batch_size,
-        num_way,
+        num_test_class,
+        num_targets_per_task,
         num_support,
         num_query,
-        num_tasks_per_epoch
+        num_tasks_per_epoch,
+        test_classes=None
 ):
     """Returns a dataloader.DataLoader for Omniglot.
 
@@ -303,28 +153,42 @@ def get_omniglot_dataloader(
             exhausted
     """
 
-    if split == 'train':
-        split_idxs = range(NUM_TRAIN_CLASSES)
-    elif split == 'val':
-        split_idxs = range(
-            NUM_TRAIN_CLASSES,
-            NUM_TRAIN_CLASSES + NUM_VAL_CLASSES
-        )
-    elif split == 'test':
-        split_idxs = range(
-            NUM_TRAIN_CLASSES + NUM_VAL_CLASSES,
-            NUM_TRAIN_CLASSES + NUM_VAL_CLASSES + NUM_TEST_CLASSES
-        )
-    else:
-        raise ValueError
+    train_dataset = OmniglotDataset(data_path,
+                                    os.path.join(data_path, 'train.csv'),
+                                    num_support, 
+                                    num_query)
+    # TODO: Add validation dataset
 
-    return dataloader.DataLoader(
-        dataset=OmniglotDataset(num_support, num_query),
+    test_dataset = OmniglotDataset(data_path,
+                                   os.path.join(data_path, 'valid.csv'),
+                                   num_support, 
+                                   num_query)
+    
+    if test_classes:
+        raise NotImplementedError
+    else:
+        classes = ChexpertDataset.chexpert_targets
+        test_idxs = np.random.choice(len(classes), num_test_class, replace=False)
+        train_idxs = [i for i in range(len(classes)) if i not in test_idxs]       
+
+    train_dataloader = dataloader.DataLoader(
+        dataset=train_dataset,
         batch_size=batch_size,
-        sampler=OmniglotSampler(split_idxs, num_way, num_tasks_per_epoch),
+        sampler=ChexpertSampler(train_idxs, num_targets_per_task, num_tasks_per_epoch),
         num_workers=2,
         collate_fn=identity,
         pin_memory=torch.cuda.is_available(),
         drop_last=True
     )
+
+    test_dataloader = dataloader.DataLoader(
+        dataset=test_dataset,
+        batch_size=batch_size,
+        sampler=ChexpertSampler(test_idxs, num_targets_per_task, num_tasks_per_epoch),
+        num_workers=2,
+        collate_fn=identity,
+        pin_memory=torch.cuda.is_available(),
+        drop_last=True
+    )
+    return train_loader, test_loader
 
